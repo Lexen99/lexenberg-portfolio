0629bd08b579c903b39279e0f2888a73720c8980
Oliver Lexenberg <o.lexenberg@hotmail.com>
2024-03-09 10:25:08 +0100

    added technologies to the logos

--- src/components/Logos/Logos.jsx
+++ src/components/Logos/Logos.jsx
@@ -15,15 +15,15 @@ const Logos = () => {
   <div className="Technologies-container">
     <h2 className="section-heading">Technologies.</h2>
     <div className="Tech-container">
-        <div className="tech-Cards"><img src={mui} alt="material-ui" /></div>
-        <div className="tech-Cards"><img src={css} alt="css" /></div>
-        <div className="tech-Cards"><img src={html} alt="html" /></div>
-        <div className="tech-Cards"><img src={JavaScript} alt="javascript" /></div>
-        <div className="tech-Cards"><img src={mongoDB} alt="mongodb" /></div>
-        <div className="tech-Cards"><img src={nodejs} alt="nodejs" /></div>
-        <div className="tech-Cards"><img src={react} alt="react" /></div>
-        <div className="tech-Cards"><img src={git} alt="git" /></div>
-        <div className="tech-Cards"><img src={express} alt="express" /></div>
+        <div className="tech-Cards"><img src={mui} alt="material-ui" /><p>MUI</p></div>
+        <div className="tech-Cards"><img src={css} alt="css" /><p>CSS</p></div>
+        <div className="tech-Cards"><img src={html} alt="html" /><p>HTML</p></div>
+        <div className="tech-Cards"><img src={JavaScript} alt="javascript" /><p>Javascript</p></div>
+        <div className="tech-Cards"><img src={mongoDB} alt="mongodb" /><p>MongoDB</p></div>
+        <div className="tech-Cards"><img src={nodejs} alt="nodejs" /><p>Node.js</p></div>
+        <div className="tech-Cards"><img src={react} alt="react" /><p>React</p></div>
+        <div className="tech-Cards"><img src={git} alt="git" /><p>Git</p></div>
+        <div className="tech-Cards"><img src={express} alt="express" /><p>Express</p></div>
     </div>
   </div>
   )
